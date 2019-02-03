c52e201526e3bfc78d519e56e6f43502fcfbbfab
Jatin Varlyani <varlyanijatin88@gmail.com>
2019-02-03 23:09:42 +0530

    Modified the wepack to use css & style loader

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,13 @@ module.exports = {
       title : 'Weather Forecaster',
       template : './src/index.html'
     })
-  ]
+  ],
+  module: {
+    rules: [
+      {
+        test:/\.css$/,
+        use:['style-loader','css-loader']
+      }
+    ]
+  }
 };
